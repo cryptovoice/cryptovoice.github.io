301b43cc19d1cd073a79abaed68a92494500463b
Arnold <cryptovoice@hotmail.com>
2018-05-31 00:36:33 +0000

    changes

--- cv-page/src/pages/index.js
+++ cv-page/src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>YO Hi people</h1>
-    <p>Welcome to your new Gatsby site. YO</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2 YO YO</Link>
+    <h1>Welcome People!</h1>
+    <p>Crypto Voice is a free for all trading community.</p>
+    <p>Join us to learn about crypto.</p>
+    <Link to="https://discord.gg/2Qb766r">Discord invitation link</Link>
   </div>
 )
 
