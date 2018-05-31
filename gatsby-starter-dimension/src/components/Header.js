bbb4263adb94db6ea6eb12589d894ea0c632c2f9
Arnold <cryptovoice@hotmail.com>
2018-05-31 06:24:35 +0000

    very nice

--- gatsby-starter-dimension/src/components/Header.js
+++ gatsby-starter-dimension/src/components/Header.js
@@ -1,26 +1,27 @@
 import React from 'react'
 
+import cvIcon from '../images/cv-icon.jpg'
+
 const Header = (props) => (
     <header id="header" style={props.timeout ? {display: 'none'} : {}}>
         <div className="logo">
-            {<span className="icon fa fa-2x fa-diamond"></span>}
+            {/*<span className="icon fa fa-2x fa-diamond"></span>*/}
             {/*<span className="icon fa-space-shuttle"></span>*/}
             {/*<span className="icon fa-eye"></span>*/}
             {/*<span className="icon fa-rocket"></span>*/}
             {/*<span className="icon fa-wheelchair-alt"></span>*/}
+            <span className="image main"><img src={cvIcon} alt="" /></span>
         </div>
         <div className="content">
             <div className="inner">
                 <h1>Crypto Voice</h1>
                 <p>Welcome to a free for all trading community</p>
-                <a href='https://discord.gg/2Qb766r'>Discord invitation link</a>
             </div>
         </div>
         <nav>
             <ul>
-                <li><a href="javascript:;" onClick={() => {props.onOpenArticle('intro')}}>Intro</a></li>
-                {/*<li><a href="javascript:;" onClick={() => {props.onOpenArticle('work')}}>Work</a></li>*/}
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('about')}}>About</a></li>
+                <li><a href='https://discord.gg/2Qb766r'>Discord</a></li>
                 <li><a href="javascript:;" onClick={() => {props.onOpenArticle('contact')}}>Contact</a></li>
             </ul>
         </nav>
