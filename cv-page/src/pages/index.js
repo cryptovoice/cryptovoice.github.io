54d8071489aa3c8b0ffa6a7e55e9b245ee90bc30
Arnold <cryptovoice@hotmail.com>
2018-05-31 00:16:43 +0000

    added gh-pages deployment

--- cv-page/src/pages/index.js
+++ cv-page/src/pages/index.js
@@ -3,10 +3,10 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>YO Hi people</h1>
+    <p>Welcome to your new Gatsby site. YO</p>
     <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/page-2/">Go to page 2 YO YO</Link>
   </div>
 )
 
